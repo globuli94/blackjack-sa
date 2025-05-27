d56af92273ec27c7a0cfca18251b68df3bae1d61
Lukas Götz <lukas.goetz@ecmind.ch>
2025-05-27 23:33:24 +0200

    Gatling Projekt-Template hinzugefügt

---
+++ performanceTesting/src/test/java/htwg/softwarearchitektur/blackjack/SaveGameScenario.java
@@ -0,0 +1,63 @@
+package htwg.softwarearchitektur.blackjack;
+
+import java.time.Duration;
+import java.util.*;
+
+import io.gatling.javaapi.core.*;
+import io.gatling.javaapi.http.*;
+import io.gatling.javaapi.jdbc.*;
+
+import static io.gatling.javaapi.core.CoreDsl.*;
+import static io.gatling.javaapi.http.HttpDsl.*;
+import static io.gatling.javaapi.jdbc.JdbcDsl.*;
+
+public class SaveGameScenario extends Simulation {
+
+  private HttpProtocolBuilder httpProtocol = http
+    .baseUrl("http://localhost:8080")
+    .inferHtmlResources()
+    .acceptHeader("*/*")
+    .acceptEncodingHeader("gzip, deflate, br")
+    .acceptLanguageHeader("de-DE,de;q=0.9,en-US;q=0.8,en;q=0.7")
+    .userAgentHeader("Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/135.0.0.0 Safari/537.36");
+  
+  private Map<CharSequence, String> headers_0 = Map.ofEntries(
+    Map.entry("Cache-Control", "no-cache"),
+    Map.entry("Origin", "http://localhost:8080"),
+    Map.entry("Pragma", "no-cache"),
+    Map.entry("Sec-Fetch-Dest", "empty"),
+    Map.entry("Sec-Fetch-Mode", "cors"),
+    Map.entry("Sec-Fetch-Site", "same-origin"),
+    Map.entry("sec-ch-ua", "Chromium\";v=\"135\", \"Not-A.Brand\";v=\"8"),
+    Map.entry("sec-ch-ua-mobile", "?0"),
+    Map.entry("sec-ch-ua-platform", "Linux")
+  );
+  
+  private Map<CharSequence, String> headers_1 = Map.ofEntries(
+    Map.entry("Cache-Control", "no-cache"),
+    Map.entry("Pragma", "no-cache"),
+    Map.entry("Sec-Fetch-Dest", "empty"),
+    Map.entry("Sec-Fetch-Mode", "cors"),
+    Map.entry("Sec-Fetch-Site", "same-origin"),
+    Map.entry("sec-ch-ua", "Chromium\";v=\"135\", \"Not-A.Brand\";v=\"8"),
+    Map.entry("sec-ch-ua-mobile", "?0"),
+    Map.entry("sec-ch-ua-platform", "Linux")
+  );
+
+
+  private ScenarioBuilder scn = scenario("SaveGameScenario")
+    .exec(
+      http("request_0")
+        .post("/game/25e9059c-e993-4d79-a921-cd2eda8a70db/save")
+        .headers(headers_0)
+        .resources(
+          http("request_1")
+            .get("/game/25e9059c-e993-4d79-a921-cd2eda8a70db/state")
+            .headers(headers_1)
+        )
+    );
+
+  {
+	  setUp(scn.injectOpen(atOnceUsers(1))).protocols(httpProtocol);
+  }
+}
